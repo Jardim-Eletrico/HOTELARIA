d1016bc05f76c9b6c4cb3d44020f2e1649788fea
Guilherme Barros <guilhermecassimiro425@gmail.com>
2026-05-12 09:01:27 -0300

    dao feita

---
+++ dao.py
@@ -0,0 +1,69 @@
+import mysql.connector
+from mysql.connector import Error
+
+class Database:
+    def __init__(self):
+        self.connection = None
+
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host = "localhost",
+                database = "hotelaria",
+                user = "root",
+                password = ""
+            )
+
+            return self.connection
+        except Error as e:
+            print(f"Erro: {e}")
+            return None
+        
+    def disconnect(self):
+        if self.connection and self.connection.is_connected():
+            self.connection.close()
+            self.connection = None
+
+    def execute_query(self, query, params=None): #INSERT, UDPATE E DELETE
+        cursor = None
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(query, params or ())
+            self.connection.commit()
+            return cursor
+        except Error as e:
+            if self.connection:
+                self.connection.rollback()
+            print(f"Erro ao executar query: {e}")
+            raise e
+        finally:
+            if cursor:
+                cursor.close()
+
+    def fetch_all(self, query, params=None): #BUSCAR VÁRIOS REGISTROS
+        cursor = None
+        try:
+            cursor = self.connection.cursor(dictionary=True)
+            cursor.execute(query, params or ())
+            result = cursor.fetchall()
+            return result
+        except Error as e:
+            print(f"Erro ao executar SELECT: {e}")
+            raise e
+        finally:
+            if cursor:
+                cursor.close()
+
+    def fetch_one(self, query, params=None): #BUSCAR APENAS UM REGISTRO
+        cursor = None
+        try:
+            cursor = self.connection.cursor(dictionary=True)
+            cursor.execute(query, params or ())
+            result = cursor.fetchone()
+            return result
+        except Error as e:
+            print(f"Erro ao executar SELECT: {e}")
+            raise e
+        finally:
+            if cursor:
+                cursor.close()
