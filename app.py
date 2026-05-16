4918b535e2ffc30b7a9a0c1f7bd9c250c06ee67f
Guilherme <guibarroscassimiro@oulook.com>
2026-05-16 19:48:12 -0300

    rota padrão feita

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ templates = Jinja2Templates(directory="templates")
 hospede_model = Hospede()
 quarto_model = Quarto()
 reserva_model = Reserva()
+
+@app.get("/")
+def root(request: Request):
+    hospedes = hospede_model.consulta_hospedes()
+    quartos = quarto_model.consulta_quartos()
+    reservas = reserva_model.consulta_reservas()
+
+    return templates.TemplateResponse("index.html", {"request": request, "hospedes": hospedes, "quartos": quartos, "reservas": reservas})
